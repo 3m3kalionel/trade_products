f8d38cdbb85220ec4e876ef9422f1cb8fda31a3f
3m3ka <emeka.l.okoro@gmail.com>
2021-02-16 13:42:14 +0100

    - install packages for sms/email messaging - create function to validate documents - create function to validate mongo ids - create function to send sms - set up email transport - create function to send email - create function for user notification - create endpoint for user notification

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,7 +2,7 @@ import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
 import userModel from "../models/userModel";
-import { handleError } from "../utils.js";
+import { handleError, notify, validateDocument } from "../utils.js";
 
 const signup = async (req, res) => {
   const { username, email, password, phoneNumber } = req.body;
@@ -85,7 +85,86 @@ const signin = async (req, res) => {
   });
 };
 
+const notifyUser = async (req, res) => {
+  const { recipientId, senderId, message } = req.body;
+
+  const returnFields = "-createdAt -updatedAt -__v";
+  const fieldsToPopulate = "";
+
+  const receiver = await validateDocument("user", recipientId, {
+    returnFields,
+    fieldsToPopulate,
+  });
+
+  if (receiver.error) {
+    const {
+      error: { code, message },
+    } = receiver;
+
+    return res.status(code).send({
+      message,
+    });
+  }
+
+  const sender = await validateDocument("user", senderId, {
+    returnFields,
+    fieldsToPopulate,
+  });
+
+  if (sender.error) {
+    const {
+      error: { code, message },
+    } = sender;
+
+    return res.status(code).send({
+      message,
+    });
+  }
+
+  let messageHtmlTemplate = `<body>
+    <div style="outline: #F3F3F7 3px solid; padding: 10px; max-width: 630px; margin: 0 auto;">
+      <div style="background-color:#F3F3F7; padding:10px; color:#BFBFC1; font-size: 12px; text-align: center; margin-bottom: 25px;">Trader Markets Sample App</div>
+      <div>
+        <div style="margin: 0 auto; max-width: 315px; font-size: 10px; color: #8893A3; flex-direction: column;">
+          <p style="color: #000"><b>Hello ${receiver.username},</b></p>
+          <p>You just received a message about one of your products. Open the web app to check it out or click the button below to open it directly.</p>
+          <div>
+          <a href="$trade-depot/reset?resetToken=#" style="margin: 25px auto; background: #EE5A33; color: #fff; width: 130px; height: 48px; border-radius: 3px; font-size: 10px;  text-decoration: none; padding: 5px 10px;">
+              Check it out
+          </a>
+          </div>
+          <span>${sender.username} - [via the web app]</span>
+          <p>message: "${message}"</p>
+        </div>
+      </div>
+      
+      <div style="background-color:#F3F3F7; padding:10px; color:#BFBFC1; font-size: 10px; text-align: center; margin-top: 25px;">
+        <p>${new Date().getFullYear()} trader_market_app Corporation. All rights reserved.</p>
+        trader_market_app Corporation<br />
+        235 Victoria Garden City, <br />
+        Lagos, NG.
+      </div>
+    </div>
+  </body>`;
+
+  let payload = {
+    receiver,
+    message,
+    email: {
+      messageHtmlTemplate,
+      subject: "Web app notification",
+    },
+  };
+
+  notify(payload);
+
+  return res.status(200).json({
+    message: "status: success - check your email for a message",
+  });
+};
+
 export default {
   signup,
   signin,
+  notifyUser,
 };
