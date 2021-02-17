49c22ce46df41328ab1a5940859aed0ae8245076
3m3ka <emeka.l.okoro@gmail.com>
2021-02-17 08:26:25 +0100

    - set up axios configuration - fix cors error - improve component styling - restructure component architecture - redesign UploadForm component - implement logic for product upload

--- client/src/components/ProgressBar.js
+++ client/src/components/ProgressBar.js
@@ -3,13 +3,14 @@ import React, { useEffect } from "react";
 import useStorage from "../hooks/useStorage";
 import "./ProgressBar.css";
 
-const ProgressBar = ({ selectedImage, setImage }) => {
+const ProgressBar = ({ selectedImage, setImage, setSavedImageUrl }) => {
   const { percentageProgress, url } = useStorage(selectedImage);
   useEffect(() => {
     if (url) {
+      setSavedImageUrl(url);
       setImage(null);
     }
-  });
+  }, [setImage, url]);
 
   return (
     <div id="progress-bar" style={{ width: percentageProgress + "%" }}></div>
--- client/src/components/UploadForm.js
+++ client/src/components/UploadForm.js
@@ -1,35 +1,119 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./UploadForm.css";
 import ProgressBar from "./ProgressBar";
+import PreviewImage from "./PreviewImage";
 
-const UploadForm = ({ selectedImage, setImage }) => {
+import axiosApi from "../api/axiosApi";
+
+const UploadForm = ({ selectedImage, setImage, previewImage }) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const [progressBarDisplay, setProgressBarDisplay] = useState(false);
+  const [savedImageUrl, setSavedImageUrl] = useState(null);
+
+  useEffect(() => {
+    async function saveProduct() {
+      await axiosApi.post("/product", {
+        name,
+        description,
+        productOwnerId: "602bafb78ca00a0b722ffaca",
+        imageUrl: savedImageUrl,
+      });
+    }
+
+    if (savedImageUrl) {
+      saveProduct();
+      toggleProgressBarDisplay();
+      setName("");
+      setDescription("");
+    }
+  }, [savedImageUrl]);
+
+  const handleNameChange = ({ target: { value } }) => {
+    setName(value);
+  };
+
+  const handleDescriptionChange = ({ target: { value } }) => {
+    setDescription(value);
+  };
+
+  const toggleProgressBarDisplay = () => {
+    setProgressBarDisplay(!progressBarDisplay);
+  };
+
   return (
     <>
-      {selectedImage && (
-        <ProgressBar selectedImage={selectedImage} setImage={setImage} />
-      )}
-      <form method="post" encType="multipart/form-data">
-        <div>
-          <label htmlFor="image_uploads">
-            Choose images to upload (PNG, JPG)
-          </label>
-          <input
-            type="file"
-            id="image_uploads"
-            name="image_uploads"
-            accept=".jpg, .jpeg, .png"
-            onChange={event => {
-              const selected = event.target.files[0];
-              setImage(selected);
-            }}
-          />
-        </div>
-        {!selectedImage && (
-          <div className="preview">
-            <p>No files currently selected for upload</p>
+      <form
+        onSubmit={event => {
+          event.preventDefault();
+          toggleProgressBarDisplay();
+        }}
+      >
+        <div id="authentication-form">
+          {selectedImage && progressBarDisplay && (
+            <ProgressBar
+              selectedImage={selectedImage}
+              setImage={setImage}
+              setSavedImageUrl={setSavedImageUrl}
+            />
+          )}
+          <div id="form-body">
+            <h1 id="form-header">Upload Product</h1>
+            <div className="form-row">
+              <input
+                name="name"
+                type="text"
+                className="auth-form-input"
+                placeholder="Product name"
+                value={name}
+                autoComplete="name"
+                onChange={handleNameChange}
+                required
+                className="upload-form-input"
+              />
+            </div>
+            <div className="form-row">
+              <input
+                name="description"
+                type="text"
+                className="auth-form-input"
+                placeholder="Product description"
+                value={description}
+                autoComplete="description"
+                onChange={handleDescriptionChange}
+                required
+                className="upload-form-input"
+              />
+            </div>
+
+            <div>
+              <label htmlFor="image_uploads">
+                Choose image to upload (PNG, JPG)
+              </label>
+              <input
+                type="file"
+                id="image_uploads"
+                name="image_uploads"
+                accept=".jpg, .jpeg, .png"
+                onChange={event => {
+                  const selected = event.target.files[0];
+                  setImage(selected);
+                }}
+              />
+            </div>
+            {!selectedImage && (
+              <div className="preview">
+                <p>No files currently selected for upload</p>
+              </div>
+            )}
+            {previewImage && <PreviewImage imageSrc={previewImage} />}
+
+            <button type="submit" id="upload-button">
+              Upload
+            </button>
           </div>
-        )}
+        </div>
       </form>
     </>
   );
