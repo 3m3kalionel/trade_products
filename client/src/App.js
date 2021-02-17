7da0c6b0e83415bf94cb39d99ef42b656f96baef
3m3ka <emeka.l.okoro@gmail.com>
2021-02-17 12:56:39 +0100

    - create Product component - add component styling for Product component - implement product list fetching in component

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import "./App.css";
 import UploadForm from "./components/UploadForm";
+import Product from "./components/Product";
+import axiosApi from "./api/axiosApi";
 
 function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [previewImage, setPreviewImage] = useState(null);
+  const [productsList, setProductsList] = useState([]);
+
+  useEffect(() => {
+    async function fetchProducts() {
+      const {
+        data: { products },
+      } = await axiosApi.get("/product");
+      setProductsList(products);
+    }
+
+    fetchProducts();
+  });
 
   const setImage = file => {
     if (file) {
@@ -19,15 +33,55 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <UploadForm
-          setImage={setImage}
-          selectedImage={selectedImage}
-          previewImage={previewImage}
-        />
-      </header>
-      <div></div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <UploadForm
+    //       setImage={setImage}
+    //       selectedImage={selectedImage}
+    //       previewImage={previewImage}
+    //     />
+    //   </header>
+    //   <div></div>
+    // </div>
+
+    <div id="full-page">
+      <div id="nav">
+        <div id="left-section">
+          <ul>
+            <li>TRADE MARKET</li>
+            <li>Shop</li>
+            <li>Learn</li>
+          </ul>
+        </div>
+        <div id="right-section">
+          <ul>
+            <li>Account</li>
+            <li onClick={() => {}}>
+              <span>NGN 400</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div id="header">
+        <div id="header-lhs">
+          <h1>All Products</h1>
+          <h4>
+            A 360<span>&#176;</span> look at Trade Market
+          </h4>
+        </div>
+        <div id="header-rhs">
+          <select disabled name="filter" id="disabled-filter">
+            <option>Filter by</option>
+          </select>
+        </div>
+      </div>
+      <div id="body">
+        <div id="body-container">
+          {productsList.map((product, key) => {
+            return <Product details={product} key={key} />;
+          })}
+        </div>
+      </div>
     </div>
   );
 }
