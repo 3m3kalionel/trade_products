46590584f82da9e490830250f8b8b9ab48c3b311
3m3ka <emeka.l.okoro@gmail.com>
2021-02-23 18:28:24 +0100

    fix heroku cors issue

--- server/app.js
+++ server/app.js
@@ -12,8 +12,29 @@ dotenv.config();
 
 export const port = process.env.PORT || 8080;
 
+const ALLOWED_ORIGINS = [
+  "http://localhost:3000",
+  "http://localhost:5000",
+  "https://trade-market-client.herokuapp.com/",
+];
+
 const app = express();
-app.use(cors());
+// app.use(cors());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+  })
+);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
