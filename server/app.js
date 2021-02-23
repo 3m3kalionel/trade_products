f925ddcbc92b020b99ba6adc7209e7e8f1378c28
3m3ka <emeka.l.okoro@gmail.com>
2021-02-23 16:22:17 +0100

    fix heroku deployment issue

--- server/app.js
+++ server/app.js
@@ -17,16 +17,16 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const publicPath = path.join(__dirname, "../build", "index.html");
-console.log("*******", publicPath);
+// const publicPath = path.join(__dirname, "../build", "index.html");
+// console.log("*******", publicPath);
 
 // app.get("/*", function (req, res) {
 //   res.sendFile(path.join(__dirname, "build", "index.html"));
 // });
 
-app.get("/*", function (req, res) {
-  res.sendFile(publicPath);
-});
+// app.get("/*", function (req, res) {
+//   res.sendFile(publicPath);
+// });
 
 app.get("/", (req, res) =>
   res.status(200).send("Yo! Welcome to trade products")
