f08bda66de03d256d67a4898bf20af5abd522ba3
3m3ka <emeka.l.okoro@gmail.com>
2021-02-23 19:38:21 +0100

    add cors config to server deploy branch

--- server/app.js
+++ server/app.js
@@ -8,9 +8,31 @@ import models from "./models";
 import connect, { getDb } from "./connect";
 
 dotenv.config();
-
 const app = express();
-app.use(cors());
+
+const ALLOWED_ORIGINS = [
+  "http://localhost:3000",
+  "http://localhost:5000",
+  "https://trade-market-client.herokuapp.com/",
+];
+
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
+  })
+);
+
+// app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
