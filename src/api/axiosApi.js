81578ddaddbc51e4a1e1ce3b439e503041cce726
3m3ka <emeka.l.okoro@gmail.com>
2021-02-23 11:52:08 +0100

    - restructure app architecture - prepare app for deployment

---
+++ src/api/axiosApi.js
@@ -0,0 +1,22 @@
+import axios from "axios";
+import dotenv from "dotenv";
+
+dotenv.config({ path: ".env" });
+
+const getbaseUrl = env => {
+  let appUrl;
+  switch (env) {
+    case "production":
+      appUrl = "REACT_APP_SERVER_PROD_URL";
+      break;
+    default:
+      appUrl = "REACT_APP_SERVER_DEV_URL";
+  }
+  return process.env[appUrl];
+};
+
+export default axios.create({
+  baseURL: getbaseUrl(process.env.NODE_ENV),
+});
+
+console.log("&&&&", process.env.NODE_ENV);
