f2701bf2b7f594ed721f3bd26c9b67fc894cf71f
3m3ka <emeka.l.okoro@gmail.com>
2021-02-27 22:43:04 +0100

    deployment

--- server/app.js
+++ server/app.js
@@ -10,36 +10,37 @@ import connect, { getDb } from "./connect";
 
 dotenv.config();
 
-export const port = process.env.PORT || 8080;
+export const port = process.env.PORT || 7000;
 
-// const ALLOWED_ORIGINS = [
-//   "http://localhost:3000",
-//   "http://localhost:5000",
-//   "https://trade-market-client.herokuapp.com/",
-//   "https://trade-market-update-client.herokuapp.com",
-//   "https://trade-market-update-client.herokuapp.com/",
-// ];
+const ALLOWED_ORIGINS = ["https://trade-market-test.herokuapp.com"];
 
 const app = express();
-// app.use(cors());
+
 app.use(
   cors({
-    origin: false,
+    origin: (origin, callback) => {
+      if (!origin) return callback(null, true);
+
+      if (ALLOWED_ORIGINS.indexOf(origin) === -1) {
+        const msg =
+          "The CORS policy for this site does not allow access from the specified Origin.";
+        return callback(new Error(msg), false);
+      }
+
+      return callback(null, true);
+    },
   })
 );
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-// const publicPath = path.join(__dirname, "../build", "index.html");
-// console.log("*******", publicPath);
+const publicPath = path.join(__dirname, "../build", "index.html");
 
-// app.get("/*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "build", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "../build")));
 
-// app.get("/*", function (req, res) {
-//   res.sendFile(publicPath);
-// });
+app.get("/*", function (req, res) {
+  res.sendFile(publicPath);
+});
 
 app.get("/", (req, res) =>
   res.status(200).send("Yo! Welcome to trade products")
